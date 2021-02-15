d4aa1518510deaf1ed6a4899d40792fcef166263
AlexParashkevov <64553049+AlexParashkevov@users.noreply.github.com>
2021-02-15 10:45:41 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,34 +6,58 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Alex Parashkevov',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
+  pin: 1337,
+  sex: 'male',
+  honorific: 'Mr.',
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Denitsa Raykova',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  sex: 'female',
+  honorific: 'Ms.',
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Ivan Parashkevov',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  sex: 'male',
+  honorific: 'Mr',
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Kristyan Popov',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  sex: 'male',
+  honorific: 'Mr.',
+};
+const account5 = {
+  owner: 'Pavlin Stefanov',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 5555,
+  sex: 'male',
+  honorific: 'Dr.',
+};
+const account6 = {
+  owner: 'Lina Stefanova',
+  movements: [4305, 10000, 7500, -5000, -90],
+  interestRate: 1,
+  pin: 1506,
+  sex: 'female',
+  honorific: 'Mrs.',
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2, account3, account4, account5, account6];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -78,7 +102,7 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements);
+
 //balance
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce(function (acc, move) {
@@ -86,29 +110,28 @@ const calcDisplayBalance = function (movements) {
   }, 0);
   labelBalance.textContent = `${balance}€`;
 };
-calcDisplayBalance(account1.movements);
 
-const caldDisplaySumm = function (movements) {
-  const incomes = movements
+const caldDisplaySumm = function (account) {
+  const incomes = account.movements
     .filter(move => move > 0)
     .reduce((acc, move) => acc + move, 0);
   labelSumIn.textContent = `${incomes}€`;
-  const out = movements
+  const out = account.movements
     .filter(move => move < 0)
     .reduce((acc, move) => acc + move, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = account.movements
     .filter(move => move > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * account.interestRate) / 100)
     .filter((int, i, arr) => {
       console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${Math.trunc(interest)}€`;
 };
-caldDisplaySumm(account1.movements);
+
 /////////////////////////////////////////////////
 //create usernames
 const createUsernames = function (acc) {
@@ -122,3 +145,39 @@ const createUsernames = function (acc) {
 };
 createUsernames(accounts);
 console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+//same thing with for of
+// for (const acc of accounts) {
+//   if (acc.owner === 'Jessica Davis') {
+//     console.log(acc);
+//   }
+// }
+//EVENT HANDLER
+let currAcc;
+btnLogin.addEventListener('click', function (event) {
+  //prevents form from submitting
+  event.preventDefault();
+  currAcc = accounts.find(acc => acc.username === inputLoginUsername.value);
+  console.log(currAcc);
+  if (currAcc?.pin === Number(inputLoginPin.value)) {
+    //display UI and welcome message
+    labelWelcome.textContent = `Welcome back, ${currAcc.honorific} ${
+      currAcc.owner.split(' ')[1]
+    }`;
+    //display movements
+    //balance
+    //summary
+    //clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    //loose focus
+    inputLoginPin.blur();
+    console.log('LOGIN');
+    containerApp.style.opacity = 100;
+    displayMovements(currAcc.movements);
+    calcDisplayBalance(currAcc.movements);
+    caldDisplaySumm(currAcc);
+  }
+});
